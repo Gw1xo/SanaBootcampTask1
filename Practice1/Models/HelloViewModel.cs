64daf05a46c4e7d60ae857578ad95c3fed92eac9
Rostislav Zhurbenko <91567031+Gw1xo@users.noreply.github.com>
2023-03-31 09:50:31 +0200

    Task 1

---
+++ Practice1/Models/HelloViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Practice1.Models;
+
+public class HelloViewModel
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string GetHello()
+    {
+        return !string.IsNullOrWhiteSpace(this.Name)
+                ? $"Hello, {this.Name}!"
+                : "Hello no name meat";
+    }
+}
